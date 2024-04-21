d6ebd3186dfb888b021032192397dfe92ad9d0d7
YASH GHARAT <53621256+Yash8452@users.noreply.github.com>
2024-04-21 13:12:45 +0530

    added components

---
+++ cities.js
@@ -0,0 +1,36 @@
+const cities = [
+  {
+    name: "New York",
+    image: "https://source.unsplash.com/random/800x600?new-york",
+  },
+  {
+    name: "Paris",
+    image: "https://source.unsplash.com/random/800x600?paris",
+  },
+  {
+    name: "Tokyo",
+    image: "https://source.unsplash.com/random/800x600?tokyo",
+  },
+  {
+    name: "London",
+    image: "https://source.unsplash.com/random/800x600?london",
+  },
+  {
+    name: "Rome",
+    image: "https://source.unsplash.com/random/800x600?rome",
+  },
+  {
+    name: "Sydney",
+    image: "https://source.unsplash.com/random/800x600?sydney",
+  },
+  {
+    name: "Dubai",
+    image: "https://source.unsplash.com/random/800x600?dubai",
+  },
+  {
+    name: "Los Angeles",
+    image: "https://source.unsplash.com/random/800x600?los-angeles",
+  },
+];
+
+export default cities;
---
+++ index.js
@@ -0,0 +1,7 @@
+import Navbar from "./Navbar.astro";
+import Hero from "./Hero.astro";
+import Planner from "./Planner.astro";
+import Cities from "./Cities.astro";
+import Footer from "./Footer.astro";
+
+export { Navbar, Hero, Planner, Cities, Footer };
